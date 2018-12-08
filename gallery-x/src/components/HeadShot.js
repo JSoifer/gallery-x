e6b9e87af2e65bf26f6d209088a704679e91fa03
Julianne Soifer <juliannesoifer@gmail.com>
2018-12-07 16:04:46 -0800

    layout header content.

--- gallery-x/src/components/HeadShot.js
+++ gallery-x/src/components/HeadShot.js
@@ -3,9 +3,8 @@ import './HeadShot.scss';
 
 function HeadShot() {
   return(
-    <div>
-      <div className='artistImage'>
-      </div>
+    <div className='headContainer'>
+      
     </div>
 
   );
--- gallery-x/src/components/Header.js
+++ gallery-x/src/components/Header.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import HeadShot from './HeadShot';
+import './Header.scss';
 
 function Header() {
   return(
-    <div>
-      <HeadShot />
-    Header
+    <div className='headerContainer'>
+      <div className='headShot'>
+        <HeadShot />
+      </div>
+      <div className='headerContent'>
+        <h1>Gallery X</h1>
+        <div className='navLinks' >
+          <div className='linkLeft'>
+            <li>about Xia</li>
+          </div>
+          <div className='rightLinks'>
+            <li>View Archived</li>
+            <li>Order Print</li>
+            <li>Contact</li>
+          </div>
+        </div>    
+      </div>
     </div>
   );
 }
